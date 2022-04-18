f9cabdf4268ae659c86ce4f8dcc67a595f528409
Rafael Almeida <rafael.rodrigo.p.a@gmail.com>
2022-04-17 22:01:40 -0300

    Consulta simples com @Query

--- src/main/java/com/rafael/mongodb/services/PostService.java
+++ src/main/java/com/rafael/mongodb/services/PostService.java
@@ -24,6 +24,6 @@ public class PostService {
 		return post;
 	}
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 }
